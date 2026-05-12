f7c830b25b8d733725893bb890bd519872d4aa8c
Jazely Tong <j8tong@ucsd.edu>
2026-05-11 21:52:58 -0700

    update

---
+++ map.js
@@ -0,0 +1,12 @@
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamF6ZWx5dG9uZyIsImEiOiJjbXAyNHhtZ2YwY2x3MnJxMm1lZGFzaHZ5In0.mCQyozrrTtID46miwkEaNg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
