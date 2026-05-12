1ad92ea7bcf0132f4231206f3e819f97fad7046e
Jazely Tong <j8tong@ucsd.edu>
2026-05-12 10:43:30 -0700

    add bluebike station markers

--- map.js
+++ map.js
@@ -1,8 +1,10 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // YOUR MAPBOX ACCESS TOKEN
-mapboxgl.accessToken = 'pk.eyJ1IjoiamF6ZWx5dG9uZyIsImEiOiJjbXAyNHhtZ2YwY2x3MnJxMm1lZGFzaHZ5In0.mCQyozrrTtID46miwkEaNg';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamF6ZWx5dG9uZyIsImEiOiJjbXAyNHhtZ2YwY2x3MnJxMm1lZGFzaHZ5In0.mCQyozrrTtID46miwkEaNg';
 
 // Create the map
 const map = new mapboxgl.Map({
@@ -13,6 +15,18 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18,
 });
+
+// Select SVG overlay
+const svg = d3.select('#map').select('svg');
+
+// Convert longitude/latitude into pixel coordinates
+function getCoords(station) {
+  const point = new mapboxgl.LngLat(+station.lon, +station.lat);
+
+  const { x, y } = map.project(point);
+
+  return { cx: x, cy: y };
+}
 
 // Wait for the map to fully load before adding data
 map.on('load', async () => {
@@ -49,4 +63,50 @@ map.on('load', async () => {
       'line-opacity': 0.6,
     },
   });
+
+  // Load Bluebikes station data
+  let jsonData;
+
+  try {
+    const jsonurl =
+      'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+
+    jsonData = await d3.json(jsonurl);
+
+    console.log('Loaded JSON Data:', jsonData);
+  } catch (error) {
+    console.error('Error loading JSON:', error);
+  }
+
+  let stations = jsonData.data.stations;
+
+  console.log('Stations Array:', stations);
+
+  // Add circles for stations
+  const circles = svg
+    .selectAll('circle')
+    .data(stations)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .attr('fill', 'steelblue')
+    .attr('stroke', 'white')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.8);
+
+  // Update circle positions
+  function updatePositions() {
+    circles
+      .attr('cx', (d) => getCoords(d).cx)
+      .attr('cy', (d) => getCoords(d).cy);
+  }
+
+  // Reposition circles when map changes
+  map.on('move', updatePositions);
+  map.on('zoom', updatePositions);
+  map.on('resize', updatePositions);
+  map.on('moveend', updatePositions);
+
+  // Initial position update
+  updatePositions();
 });
